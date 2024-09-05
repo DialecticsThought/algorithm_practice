d5964f6f3736e11ccded73c2bc54b53f25443ba3
veritas <1820819809@qq.com>
2024-09-05 22:06:35 +0800

    update

--- src/main/java/heima_data_structure/java/src/main/java/com/itheima/algorithm/greedy/Code_322_ChangeMakingProblem.java
+++ src/main/java/heima_data_structure/java/src/main/java/com/itheima/algorithm/greedy/Code_322_ChangeMakingProblem.java
@@ -4,7 +4,7 @@ package heima_data_structure.java.src.main.java.com.itheima.algorithm.greedy;
  * <h3>零钱兑换 - 贪心解法</h3>
  * <p>贪心解法：可能得到错误的解</p>
  */
-public class ChangeMakingProblemLeetcode322 {
+public class Code_322_ChangeMakingProblem {
     /*
             总金额 18
             5   2   1
@@ -59,7 +59,7 @@ public class ChangeMakingProblemLeetcode322 {
     }
 
     public static void main(String[] args) {
-        ChangeMakingProblemLeetcode322 leetcode = new ChangeMakingProblemLeetcode322();
+        Code_322_ChangeMakingProblem leetcode = new Code_322_ChangeMakingProblem();
 //        int count = leetcode.coinChange(new int[]{5, 2, 1}, 18);
 //        int count = leetcode.coinChange(new int[]{25, 10, 5, 1}, 41);
 //        int count = leetcode.coinChange(new int[]{2}, 3);
