013495ea5337615f7c942755e67db9e7867f73c2
veritas <1820819809@qq.com>
2024-10-12 14:48:03 +0800

    update

--- src/main/java/Code_132_PalindromePartitioningII.java
+++ src/main/java/Code_132_PalindromePartitioningII.java
@@ -12,33 +12,33 @@ public class Code_132_PalindromePartitioningII {
      * Str = "aabcbdd"
      *                                      [0]
      *                      是回文  /    是回文 |         \ 不是回文
-     *                  [a]+minCuts(1)  [aa]+minCuts(2)  [aab]
+     *                  [a]+f(1)  [aa]+f(2)  [aab]
      *                  是回文 /      \ 不是回文
-     *              [a]+minCuts(2)    [ab]
+     *              [a]+f(2)    [ab]
      *              是回文 /    \ 是回文
-     *          [b]+minCuts(3)  [bcb]+minCuts(4)
+     *          [b]+f(3)  [bcb]+f(4)
      *          是回文 /       \ 不是回文
-     *      [c]+minCuts(4)     [cb]
+     *      [c]+f(4)     [cb]
      *     是回文 /        \ 不是回文
-     *   [b]+minCuts(5)  [bd]
+     *   [b]+f(5)  [bd]
      *  是回文/        \是回文
-     *  [d]+minCuts(6) [dd]+minCuts(7)
+     *  [d]+f(6) [dd]+f(7)
      *    / 是回文
-     *  [d]+minCuts(7)
+     *  [d]+f(7)
      * </pre>
      * @param s
      * @return
      */
     public int minCut(String s) {
-        return minCuts(s, 0);
+        return f(s, 0);
     }
 
-    private int minCuts(String s, int start) {
+    private int f(String s, int start) {
         if (start == s.length()) {
             return 0; // 如果已经到达字符串末尾，不需要再切割
         }
 
-        int minCuts = Integer.MAX_VALUE;
+        int f = Integer.MAX_VALUE;
 
         for (int end = start; end < s.length(); end++) {
             /**
@@ -48,12 +48,12 @@ public class Code_132_PalindromePartitioningII {
              */
             if (isPalindrome(s, start, end)) {
                 // 计算剩余部分的最小切割次数
-                int cuts = 1 + minCuts(s, end + 1);
+                int cuts = 1 + f(s, end + 1);
                 // 取最小的切割次数
-                minCuts = Math.min(minCuts, cuts);
+                f = Math.min(f, cuts);
             }
         }
-        return minCuts;
+        return f;
     }
 
     // 辅助函数：判断子字符串 s[start:end] 是否是回文
