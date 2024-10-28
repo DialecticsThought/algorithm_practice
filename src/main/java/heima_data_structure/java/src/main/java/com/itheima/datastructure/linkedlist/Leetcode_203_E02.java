6ddebd35e539bcc8e1a125e279e6fbb1e681ee8a
veritas <1820819809@qq.com>
2024-10-28 17:07:13 +0800

    update

--- src/main/java/heima_data_structure/java/src/main/java/com/itheima/datastructure/linkedlist/Leetcode_203_E02.java
+++ src/main/java/heima_data_structure/java/src/main/java/com/itheima/datastructure/linkedlist/Leetcode_203_E02.java
@@ -50,18 +50,74 @@ public class Leetcode_203_E02 {
 
     /**
      * 方法2
+     * <pre>
+     *  TODO 递归含义： 从当前节点（我）开始，完成删除的子链表 ，返回删除之后的子链表头结点
+     *  eg:
+     *   1 -> 2 -> 3 -> 4
+     *   删除2
+     *    removeElements(1 -> 2 -> 3 -> 4, 2)
+     *      │
+     *      ├── 不匹配，继续递归调用
+     *      │   │
+     *      │   └─ removeElements(2 -> 3 -> 4, 2)
+     *      │       │
+     *      │       ├── 匹配，跳过当前节点，继续递归调用
+     *      │       │   │
+     *      │       │   └─ removeElements(3 -> 4, 2)
+     *      │       │       │
+     *      │       │       ├── 不匹配，继续递归调用
+     *      │       │       │   │
+     *      │       │       │   └─ removeElements(4, 2)
+     *      │       │       │       │
+     *      │       │       │       ├── 不匹配，继续递归调用
+     *      │       │       │       │   │
+     *      │       │       │       │   └─ removeElements(null, 2)
+     *      │       │       │       │       │
+     *      │       │       │       │       └─ 返回 null
+     *      │       │       │       │
+     *      │       │       │       └─ 返回 4 -> null
+     *      │       │       │
+     *      │       │       └─ 返回 3 -> 4 -> null
+     *      │       │
+     *      │       └─ 返回 3 -> 4 -> null
+     *      │
+     *      └─ 返回 1 -> 3 -> 4 -> null
+     *   调用过程
+     *      初始调用：removeElements(head, 2)
+     *      当前节点值为 1，不等于 2。
+     *      递归调用 removeElements(head.next, 2)，并将 head.next 更新为这个结果。
      *
+     *      第二次调用：removeElements(2 -> 3 -> 4, 2)
+     *      当前节点值为 2，等于 2。
+     *      跳过当前节点，直接递归调用 removeElements(3 -> 4, 2)。
+     *
+     *      第三次调用：removeElements(3 -> 4, 2)
+     *      当前节点值为 3，不等于 2。
+     *      递归调用 removeElements(4, 2)，并将 head.next 更新为这个结果。
+     *
+     *      第四次调用：removeElements(4, 2)
+     *      当前节点值为 4，不等于 2。
+     *      递归调用 removeElements(null, 2)，并将 head.next 更新为这个结果。
+     *
+     *      第五次调用（基准情况）：removeElements(null, 2)
+     *      当前节点是 null，表示已到达链表末尾。
+     *      返回 null。
+     * </pre>
      * @param p   链表头
      * @param val 目标值
      * @return 删除后的链表头
      */
     public ListNode removeElements(ListNode p, int val) {
+        // base case 如果当前节点是null 说明来到链表的结尾了
         if (p == null) {
             return null;
         }
+        // 若被遍历的节点与 v 相等，应该返回下一个节点递归结果
         if (p.val == val) {
+            // 直接返回 removeElements(p.next, val)，即跳过当前节点，将后续节点中的值为 val 的节点继续删除
             return removeElements(p.next, val);
-        } else {
+        } else {// 若我与 v 不等，则将当前被遍历的节点保留在链表中 应该返回当前被遍历的节点
+            // 并将当前被遍历的节点的 next 应该更新 为 后续删过的子链表的链头
             p.next = removeElements(p.next, val);
             return p;
         }
