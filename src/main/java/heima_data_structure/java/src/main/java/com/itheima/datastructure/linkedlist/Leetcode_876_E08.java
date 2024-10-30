b0b08b165728cc82797e73bc8f60fea1f6493c90
veritas <1820819809@qq.com>
2024-10-30 14:55:07 +0800

    update

--- src/main/java/heima_data_structure/java/src/main/java/com/itheima/datastructure/linkedlist/Leetcode_876_E08.java
+++ src/main/java/heima_data_structure/java/src/main/java/com/itheima/datastructure/linkedlist/Leetcode_876_E08.java
@@ -5,18 +5,47 @@ package heima_data_structure.java.src.main.java.com.itheima.datastructure.linked
  */
 public class Leetcode_876_E08 {
 
-    /*
-                p1
-                        p2
-        1   2   3   4   5   null
-
-
-                    p1
-                                p2
-        1   2   3   4   5   6   null
-     */
-
     /**
+     * <pre>
+     *   利用快慢指针
+     *      慢指针一次走一步
+     *      快指针一次走两步
+     *      初始情况：快慢指针都在初始节点
+     *
+     *      1.针对奇数节点
+     *         p1
+     *         p2
+     *         1   2   3   4   5   null
+     *
+     *             p1
+     *                 p2
+     *         1   2   3   4   5   null
+     *
+     *                   p1
+     *                         p2
+     *         1   2   3   4   5   null
+     *         此时 快指针p2所指向的节点的next指针已经是null，说明p1已经指向了 需要指向的中间节点
+     *
+     *         2.针对奇数节点
+     *         p1
+     *         p2
+     *         1   2   3   4   5   6   null
+     *
+     *             p1
+     *                 p2
+     *         1   2   3   4   5   6   null
+     *
+     *                 p1
+     *                         p2
+     *         1   2   3   4   5   6   null
+     *
+     *                     p1
+     *                                 p2
+     *         1   2   3   4   5   6   null
+     *
+     *         此时 快指针p2所指向的节点已经是null，说明p1已经指向了 需要指向的中间节点
+     * </pre>
+     *
      * @param head 头节点
      * @return 中间节点
      */
